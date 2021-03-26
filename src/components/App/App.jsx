e8f89eb9e9782d966fdb187c8466bb3957b38de7
alex braatz <alexjbraatz@gmail.com>
2021-03-26 16:23:13 -0500

    begin axios GET route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  let [ gallery, setGallery ] = useState([]);
+
+  useEffect( ()=>{
+    console.log( 'in useEffect' );
+    getGallery();
+  });
+
+  const getGallery = () => {
+    axios.get( '/gallery' ).then( ( response )=>{
+      console.log( 'getGallery response:', response.data );
+      setGallery( response.data );
+    }).catch( ( error )=>{
+      console.log( error );
+      alert( 'not today amigo' );
+    })
+  }
+
+
     return (
       <div className="App">
         <header className="App-header">
