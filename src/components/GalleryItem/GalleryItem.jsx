d564ca8e8808a64cd0bab8b0e907468f66acfd95
alex braatz <alexjbraatz@gmail.com>
2021-03-26 17:21:27 -0500

    get photo library path rendering to DOM

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 
 function GalleryItem( props ){
-
+    console.log( 'galleryItem', props );
+    return(
+        <>
+        <li>{props.item.path}</li>
+        </>
+    )
 };
 
 export default GalleryItem;
